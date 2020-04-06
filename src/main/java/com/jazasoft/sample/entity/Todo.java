3187fd5d65a6e95317d19ab28c1a4965750e7c50
razamd <md-zahid.raza@outlook.com>
2020-04-06 20:09:45 +0530

    API enhanced to use physical database.

--- src/main/java/com/jazasoft/sample/entity/Todo.java
+++ src/main/java/com/jazasoft/sample/entity/Todo.java
@@ -3,10 +3,18 @@ package com.jazasoft.sample.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @NoArgsConstructor
 @Data
+@Entity
 public class Todo {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
---
+++ src/main/java/com/jazasoft/sample/service/InMemoryTodoService.java
@@ -0,0 +1,58 @@
+package com.jazasoft.sample.service;
+
+import com.jazasoft.sample.entity.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+//@Service
+public class InMemoryTodoService implements TodoService {
+  private Map<Long, Todo> todoCache = new HashMap<>();
+  long counter = 0;
+
+  public InMemoryTodoService() {
+    Todo todo1 = new Todo(1L, "Go to Shopping");
+    Todo todo2 = new Todo(2L, "Watch Movie");
+
+    todoCache.put(todo1.getId(), todo1);
+    todoCache.put(todo2.getId(), todo2);
+
+    counter = 3;
+  }
+
+  @Override
+  public Collection<Todo> findAll() {
+    return todoCache.values();
+  }
+
+  @Override
+  public Todo findOne(Long id) {
+    return todoCache.get(id);
+  }
+
+  @Override
+  public Todo save(Todo todo) {
+    todo.setId(counter++);
+    todoCache.put(todo.getId(), todo);
+    return todo;
+  }
+
+  @Override
+  public Todo update(Todo todo) {
+    Todo mTodo = todoCache.get(todo.getId());
+    mTodo.setName(todo.getName());
+    return mTodo;
+  }
+
+  @Override
+  public void delete(Long id) {
+    todoCache.remove(id);
+  }
+
+  @Override
+  public boolean exists(Long id) {
+    return todoCache.containsKey(id);
+  }
+}
